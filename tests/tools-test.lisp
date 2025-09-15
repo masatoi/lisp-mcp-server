48929029f9be9b0e0ef6d818a546702bf9b54236
cxxxr <g23tlm@gmail.com>
2025-09-16 00:50:47 +0900

    test(tools): temporarily limit to tools/list test to get red-green cycle started

--- tests/tools-test.lisp
+++ tests/tools-test.lisp
@@ -23,16 +23,4 @@
             (ok (string= (gethash "type" schema) "object"))
             (ok (string= (gethash "type" code) "string")))))))
 
-(deftest tools-call-repl-eval
-  (testing "tools/call executes repl.eval and returns text content"
-    (let* ((req "{\"jsonrpc\":\"2.0\",\"id\":2,\"method\":\"tools/call\",\"params\":{\"name\":\"repl.eval\",\"arguments\":{\"code\":\"(+ 1 2)\"}}}"))
-      (let* ((resp (mcp:process-json-line req))
-             (obj (yason:parse resp))
-             (result (gethash "result" obj))
-             (content (and result (gethash "content" result)))
-             (first (and (arrayp content) (> (length content) 0) (aref content 0))))
-        (ok (string= (gethash "jsonrpc" obj) "2.0"))
-        (ok (eql (gethash "id" obj) 2))
-        (ok (arrayp content))
-        (ok (string= (gethash "type" first) "text"))
-        (ok (string= (gethash "text" first) "3"))))))
+;; tools/call test to be added after tools/list passes
